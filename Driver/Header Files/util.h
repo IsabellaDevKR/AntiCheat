5288d2064f702768d16e61c20976124bf7de7465
Animbella-Software <alexhusum7@gmail.com>
2024-10-16 18:01:58 +0100

    Add files via upload

---
+++ Driver/Header Files/util.h
@@ -0,0 +1,23 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "common.h"
+
+LARGE_INTEGER
+GenerateRandSeed();
+
+NTSTATUS
+MapAndReadPhysical(_In_ UINT64 PhysicalAddress, 
+	               _In_ UINT32 ReadLength,
+	               _Out_ PVOID OutputBuffer,
+	               _In_ UINT32 OutputBufferLength);
+
+NTSTATUS
+UnicodeToCharBufString(_In_ PUNICODE_STRING UnicodeString,
+	                   _Out_ PVOID          OutBuffer,
+	                   _In_ UINT32          OutBufferSize);
+
+VOID
+DumpBufferToKernelDebugger(_In_ PCHAR Buffer, _In_ UINT32 BufferLength);
+
+#endif
